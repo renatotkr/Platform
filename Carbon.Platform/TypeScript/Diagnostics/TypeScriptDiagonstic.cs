2d4564f66f2461123e0fa032e6ebadd179b9df97
Jason Nelson <jason@carbonmade.com>
2015-10-19 17:45:06 -0500

    Add TypeScript Compiler

---
+++ Carbon.Platform/TypeScript/Diagnostics/TypeScriptDiagonstic.cs
@@ -0,0 +1,71 @@
+﻿namespace TypeScript
+{
+    using System;
+    using System.Collections.ObjectModel;
+    using System.IO;
+
+    using Carbon.Platform;
+
+    public class TypeScriptDiagonstic : Diagnostic
+    {
+        public static TypeScriptDiagonstic Parse(string text)
+        {
+            // D:/builds/93ef00b2abe44ca8858603bef9fa0c78/5e31664ea2831739542b0607c0183b74f38dbdee78221b5ac944edc52f70a95b.d.ts(103,5)
+            // : error TS2300
+            // : Duplicate identifier 'jsonp'.
+
+            var parts = text.Split(new[] { ": " }, StringSplitOptions.RemoveEmptyEntries);
+
+            var error = new TypeScriptDiagonstic();
+
+            if (parts.Length < 2)
+            {
+                error.Message = text;
+
+                return error;
+            }
+
+            try
+            {
+                var fileParts = parts[0].TrimEnd(')').Split('(');
+                var positionParts = fileParts[1].Split(',');
+
+                error.FileName = fileParts[0];
+                error.Line = int.Parse(positionParts[0]);
+                error.Column = int.Parse(positionParts[1]);
+                error.Code = parts[1].Replace("error", "");
+
+                if (parts.Length > 2)
+                {
+                    error.Message = parts[2];
+                }
+            }
+            catch
+            {
+                throw new Exception(parts.Length + ":" + text);
+            }
+
+            return error;
+        }
+    }
+
+    public class ErrorList : Collection<TypeScriptDiagonstic>
+    {
+        public static ErrorList Parse(string text)
+        {
+            var list = new ErrorList();
+
+            string line;
+
+            using (var reader = new StringReader(text))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    list.Add(TypeScriptDiagonstic.Parse(line));
+                }
+            }
+
+            return list;
+        }
+    }
+}
