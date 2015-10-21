7ccfa2a37c1d73f7c16b62c9229d147452eb6a8f
Jason Nelson <jason@carbonmade.com>
2015-10-21 17:00:32 -0500

    Add BitBucket Repository

---
+++ Carbon.Platform/Repositories/Providers/BitBucket/BitBucketClient.cs
@@ -0,0 +1,100 @@
+﻿namespace Bitbucket
+{
+    using System;
+    using System.IO;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Carbon.Data;
+
+    public class BitbucketClient
+    {
+        const string baseUri = "https://bitbucket.org/api/2.0";
+
+        private readonly HttpClient httpClient = new HttpClient();
+
+        private readonly string userName;
+        private readonly string password;
+
+        public BitbucketClient(string userName, string password)
+        {
+            this.userName = userName;
+            this.password = password;
+        }
+
+        public async Task<BitbucketCommit> GetCommit(string accountName, string repoName, string revision)
+        {
+            // https://bitbucket.org/api/2.0
+
+            // POST /repositories/carbonmade/mason/commits/master
+
+            var path = $"/repositories/{accountName}/{repoName}/commits/{revision}";
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, baseUri + path);
+
+
+            var response = await Send(httpRequest).ConfigureAwait(false);
+
+            return response["values"][0].As<BitbucketCommit>();
+        }
+
+        public async Task<MemoryStream> GetZipStream(string accountName, string repoName, string revision)
+        {
+            var url = $"https://bitbucket.org/{accountName}/{repoName}/get/{revision}.zip";
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, url);
+
+            httpRequest.Headers.UserAgent.Add(new ProductInfoHeaderValue("Carbon", "1.0.0"));
+
+            httpRequest.Headers.Authorization = new AuthenticationHeaderValue(
+                scheme: "Basic",
+                parameter: Convert.ToBase64String(Encoding.ASCII.GetBytes($"{userName}:{password}"))
+            );
+
+            using (var response = await httpClient.SendAsync(httpRequest).ConfigureAwait(false))
+            {
+                if (!response.IsSuccessStatusCode)
+                {
+                    var responseText = response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    throw new Exception(responseText + " : " + httpRequest.RequestUri.ToString());
+                }
+                
+                var ms = new MemoryStream();
+
+                using (var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                {
+                    await stream.CopyToAsync(ms).ConfigureAwait(false);
+                }
+
+                ms.Position = 0;
+
+                return ms;
+            }
+        }
+
+        public async Task<XNode> Send(HttpRequestMessage httpRequest)
+        {
+            httpRequest.Headers.UserAgent.Add(new ProductInfoHeaderValue("Carbon", "1.0.0"));
+
+            httpRequest.Headers.Authorization = new AuthenticationHeaderValue(
+                scheme: "Basic",
+                parameter: Convert.ToBase64String(Encoding.ASCII.GetBytes($"{userName}:{password}"))
+            );
+
+            using (var response = await httpClient.SendAsync(httpRequest).ConfigureAwait(false))
+            {
+                var responseText = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(responseText + " : " + httpRequest.RequestUri.ToString());
+                }
+
+                return XObject.Parse(responseText);
+            }
+        }
+    }
+}
---
+++ Carbon.Platform/Repositories/Providers/BitBucket/BitBucketRepository.cs
@@ -0,0 +1,61 @@
+﻿namespace Bitbucket
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Carbon.Platform;
+
+    public class BitbucketRepository : IRepositoryClient
+    {
+        private readonly string accountName;
+        private readonly string repositoryName;
+
+        private readonly BitbucketClient client;
+
+        public BitbucketRepository(Uri url, string userName, string password)
+        {
+            #region Preconditions
+
+            if (url == null) throw new ArgumentNullException(nameof(url));
+            if (userName == null) throw new ArgumentNullException(nameof(userName));
+            if (password == null) throw new ArgumentNullException(nameof(password));
+
+            #endregion
+
+            // https://bitbucket.org/carbonmade/lefty.git
+
+            var path = url.AbsolutePath.Replace(".git", "");
+            var split = path.Trim('/').Split('/');
+
+            this.accountName = split[0];
+            this.repositoryName = split[1];
+
+            this.client = new BitbucketClient(userName, password);
+        }
+
+        public async Task<ICommit> GetCommitAsync(Revision revision)
+        {
+            var commit = await client.GetCommit(accountName, repositoryName, revision.Name).ConfigureAwait(false);
+
+            if (commit == null)
+            {
+                throw new Exception($"The repository '{repositoryName}' does not have a reference named '{revision.Path}'");
+            }
+
+            return commit;
+        }
+
+
+        public Task TagAsync(ICommit commit, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Package> DownloadAsync(Revision revision)
+        {
+            var stream = await client.GetZipStream(accountName, repositoryName, revision.Name).ConfigureAwait(false);
+
+            return ZipPackage.FromStream(stream, stripFirstLevel: true);
+        }
+    }
+}
