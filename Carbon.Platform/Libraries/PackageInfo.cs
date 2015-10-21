5e48098c8b224a2f13423139f8947299ee87e5e3
Jason Nelson <jason@carbonmade.com>
2015-10-21 17:02:05 -0500

    Add PackageInfo

---
+++ Carbon.Platform/Libraries/PackageInfo.cs
@@ -0,0 +1,33 @@
+﻿namespace Carbon.Platform
+{
+    using System.Collections.Generic;
+
+    using Carbon.Data;
+
+    public class PackageInfo
+    {
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string Description { get; set; }
+
+        public string License { get; set; }
+
+        public string Main { get; set; }
+
+        // public PackageRepository Repository { get; set; }
+
+        public StringMap Dependencies { get; set; }
+
+        public static PackageInfo Parse(string text)
+        {            
+            return XObject.Parse(text).As<PackageInfo>();
+        }
+
+        public static PackageInfo Parse(IAsset asset)
+        {
+            return Parse(asset.ReadStringAsync().ToString());
+        }
+    }
+}
