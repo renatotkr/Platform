3a31f672ca414d9da0b8dfa3f2a706848ff622f7
Jason Nelson <jason@carbonmade.com>
2015-10-21 16:59:57 -0500

    Simplify Revision

--- Carbon.Platform/Repositories/Revision.cs
+++ Carbon.Platform/Repositories/Revision.cs
@@ -1,125 +1,93 @@
 ﻿namespace Carbon.Platform
 {
-	using System;
+    using System;
 
-	// AKA (ref)
+    public class Revision
+    {
+        public static readonly Revision Master = new Revision("master", RefType.Head);
 
-	public class Revision
-	{
-		public static readonly Revision Master = new Revision("master", RefType.Head);
+        public Revision(string name, RefType type)
+        {
+            #region Preconditions
 
-		private readonly string name;
-		private readonly RefType type;
+            if (name == null) throw new ArgumentNullException(nameof(name));
 
-		public Revision(string name, RefType type)
-		{
-			#region Preconditions
+            #endregion
 
-			if (name == null) throw new ArgumentNullException("name");
+            Name = name;
+            Type = type;
+        }
 
-			#endregion
+        public string Name { get; }
 
-			this.name = name;
-			this.type = type;
-		}
+        public RefType Type { get; }
 
-		public string Name => name;
+        public string Path
+        {
+            get
+            {
+                switch (Type)
+                {
+                    case RefType.Commit : return Name;
+                    case RefType.Tag    : return "tags/" + Name;
+                    case RefType.Head   : return "heads/" + Name;
+                    default             : throw new Exception("Unexpected refType");
+                }
+            }
+        }
 
-		public RefType Type => type;
+        public override string ToString() => Path;
 
-		public string Path
-		{
-			get
-			{
-				switch (Type)
-				{
-					case RefType.Commit	: return Name;
-					case RefType.Tag	: return "tags/" + Name;
-					case RefType.Head	: return "heads/" + Name;
-					default			    : throw new Exception("Unexpected refType");
-				}
-			}
-		}
+        public static Revision Parse(string text)
+        {
+            #region Preconditions
 
-		public override string ToString() => Path;
+            if (text == null) throw new ArgumentNullException(nameof(text));
 
-		public static Revision Parse(string text)
-		{
-			#region Preconditions
+            #endregion
 
-			if (text == null) throw new ArgumentNullException("text");
+            var type = RefType.Head;
+            var name = "";
 
-			#endregion
+            var parts = text.Split('/');
 
-			var type = RefType.Head;
-			var name = "";
+            if (parts.Length == 1)
+            {
+                name = parts[0];
 
-			var parts = text.Split('/');
+                // revision (40 byte, 20 character hexidecimal, SHA1 or a name that denotes a particular object)
+                // dae86e1950b1277e545cee180551750029cfe735
 
-			if (parts.Length == 1)
-			{
-				name = parts[0];
+                if (name.Length == 20)
+                {
+                    // It's a commit ref
+                    return new Revision(name, RefType.Commit);
+                }
 
-				// revision (40 byte, 20 character hexidecimal, SHA1 or a name that denotes a particular object)
-				// dae86e1950b1277e545cee180551750029cfe735
+                // Otherwise, it's a symbolic ref name to a specific revision
+            }
+            else if (parts.Length == 2)
+            {
+                name = parts[1];
 
-				if (name.Length == 20) 
-				{
-					// It's a commit ref
-					return new Revision(name, RefType.Commit);
-				}
+                switch (parts[0])
+                {
+                    case "tags"     : type = RefType.Tag; break;
+                    case "branches" : type = RefType.Head; break;
+                    case "heads"    : type = RefType.Head; break;
 
-				// Otherwise, it's a symbolic ref name to a specific revision
-			}
-			else if(parts.Length == 2)
-			{
-				name = parts[1];
+                    default: throw new Exception("Unexpected type");
+                }
+            }
 
-				switch (parts[0])
-				{
-					case "tags":		type = RefType.Tag;		break;
-					case "branches":	type = RefType.Head;	break;
-					case "heads":		type = RefType.Head;	break;
+            return new Revision(name, type);
+        }
+    }
 
-					default:			throw new Exception("Unexpected type");
-				}
-			}
-
-			return new Revision(name, type);
-		}
-	}
-
-	public enum RefType : byte
-	{
-		Commit = 1,
-		Head = 2,
-		Tag = 3
-	}
-
-
-	// Deployments are a request for a specific ref(branch,sha,tag) to be deployed.
+    public enum RefType
+    {
+        Commit = 1,
+        Head = 2,
+        Tag = 3
+    }
 }
-
-/*
- tags		reside in refs/tags/ namespace
- branches	reside in refs/heads/ namespace
-
- master - development version, soon to be 1.1
- heads/1.0 - 1.0.0 release version
- tags/1.0.0 - 1.0.0 release version
- 
- A head is simply a reference to a commit object. 
- Each head has a name (branch name or tag name, etc). 
- By default, there is a head in every repository called master. 
- A repository can contain any number of heads.
- At any given time, one head is selected as the “current head.”
- This head is aliased to HEAD, always in capitals".
-
- Note this difference: a “head” (lowercase) refers to any one of the named heads in the repository;
- “HEAD” (uppercase) refers exclusively to the currently active head.
- This distinction is used frequently in Git documentation.
-
- In contrast to a head, a tag is not updated by the commit command.
-
- A symbolic ref name. E.g. master typically means the commit object referenced by refs/heads/master. 
-*/
