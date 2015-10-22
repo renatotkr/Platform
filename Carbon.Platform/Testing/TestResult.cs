5a8d42ff1d7a4b87b5eb1e1809d775847814c1fa
Jason Nelson <jason@carbonmade.com>
2015-10-22 17:50:16 -0500

    Add TestRun helpers

---
+++ Carbon.Platform/Testing/TestResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Carbon.Platform
+{
+	public enum TestResult
+	{
+		Unknown = 0,
+		Passed = 1,
+		Failed = 2
+	}
+}
---
+++ Carbon.Platform/Testing/TestRun.cs
@@ -0,0 +1,48 @@
+﻿namespace Carbon.Platform
+{
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+
+	[Table("TestRuns")]
+	public class TestFixtureRun
+	{
+		[Column("testName"), Key]
+		public string TestName { get; set; }
+	
+		[Column("id"), Key]
+		public long Id { get; set; }
+
+		[Column("browser")]
+		public string Browser { get; set; }
+
+		[Column("status")]
+		public TestResult Status { get; set; }
+
+		[Column("date")]
+		public DateTime Date { get; set; }
+
+		[Column("duration")]
+		public int Duration { get; set; } // In ms
+
+		[Column("passed")]
+		public int Passed { get; set; }
+
+		[Column("total")]
+		public int Total { get; set; }
+
+		[Column("module")]
+		public string Module { get; set; } // To group smaller tests
+	}
+}
+
+/*
+Object 
+duration: 2
+failed: 0
+module: undefined
+name: "Triggers"
+passed: 1
+total: 1
+__proto__: Object
+*/
