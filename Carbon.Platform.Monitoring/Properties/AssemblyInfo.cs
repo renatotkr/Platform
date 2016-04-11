262ab95a4e566456199b17a9b56c6e834d41aed0
Jason Nelson <jason@carbonmade.com>
2016-04-10 20:43:51 -0500

    Move monitoring to Carbon.Supervisor

--- Carbon.Platform.Monitoring/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Carbon Platform.Monitoring")]
-[assembly: AssemblyProduct("Carbon")]
-[assembly: AssemblyCopyright("© 2015 Jason Nelson")]
-
-[assembly: AssemblyVersion("0.1.0")]
