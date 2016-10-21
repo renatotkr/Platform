37b9817cdcae2bc0b185f45b7420dfd4b97a0587
Jason Nelson <jason@carbonmade.com>
2016-10-20 19:49:49 -0500

    Add buildscript

---
+++ build.ps1
@@ -0,0 +1,3 @@
+dotnet pack ./Carbon.Platform         -o ./build/Carbon.Platform/
+dotnet pack ./Carbon.Packaging        -o ./build/Carbon.Packaging/
+dotnet pack ./Carbon.Hosting          -o ./build/Carbon.Hosting/
