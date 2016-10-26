96faf89c421e3c0a093a9108cfb5cfe6eaae8427
Jason Nelson <jason@carbonmade.com>
2016-10-26 15:51:20 -0500

    Move builder to building

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+dotnet pack ./Carbon.Building         -o ./build/Carbon.Building/
 dotnet pack ./Carbon.Platform         -o ./build/Carbon.Platform/
 dotnet pack ./Carbon.Packaging        -o ./build/Carbon.Packaging/
 dotnet pack ./Carbon.Hosting          -o ./build/Carbon.Hosting/
