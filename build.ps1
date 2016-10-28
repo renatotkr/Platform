b4efe675d0e04e1ea414730618db000794645e19
Jason Nelson <jason@carbonmade.com>
2016-10-27 20:36:39 -0500

    Update buildscript

--- build.ps1
+++ build.ps1
@@ -2,3 +2,6 @@ dotnet pack ./Carbon.Building         -o ./build/Carbon.Building/
 dotnet pack ./Carbon.Platform         -o ./build/Carbon.Platform/
 dotnet pack ./Carbon.Packaging        -o ./build/Carbon.Packaging/
 dotnet pack ./Carbon.Hosting          -o ./build/Carbon.Hosting/
+dotnet pack ./Carbon.Hosting.IIS      -o ./build/Carbon.Hosting.IIS/
+dotnet pack ./Carbon.Runtime          -o ./build/Carbon.Runtime/
+dotnet pack ./Carbon.Building         -o ./build/Carbon.Building/
