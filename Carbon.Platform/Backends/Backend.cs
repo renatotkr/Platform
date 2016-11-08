99ba124438093e08ecae7e58349d25c33ddfb04e
Jason Nelson <jason@carbonmade.com>
2016-11-07 20:48:04 -0600

    Rename apps back to backends

---
+++ Carbon.Platform/Backends/Backend.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Carbon.Computing
+{
+    using Data.Annotations;
+
+    [Dataset("Backends")]
+    public class Backend
+    {
+        [Member(1), Key]  // Assigned (1-1 w/ Program)
+        public long Id { get; set; }
+
+        [Member(2), Unique]
+        [StringLength(50)]
+        public string Name { get; set; } // e.g. carbonmade
+
+        [Member(3)]
+        public NetworkPortList Listeners { get; set; } // e.g. 80/http
+
+        [Member(4)]
+        public long ProgramId { get; set; }         //  5
+
+        [Member(5), Mutable]
+        public Semver Version { get; set; }    // Active version
+        
+        // Hosts?
+
+        public IList<BackendInstance> Instances { get; set; }
+    }
+}
+
+// An backend exposes a program as a load balanced service
+
+// - spawns one or more instances to handle user requests (autoscaling)
+// - hosted behind a load balancer
+
+// bindings (host, protocal, port)
---
+++ Carbon.Platform/Backends/BackendInstance.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Carbon.Computing
+{
+    using Data.Annotations;
+
+    [Dataset("BackendInstances")]
+    public class BackendInstance
+    {        
+        [Member(1), Key]
+        public long BackendId { get; set; }
+
+        [Member(2), Key]
+        public long HostId { get; set; }
+
+        [Member(3), Mutable] // Program version
+        public Semver Version { get; set; }
+
+        [Member(4), Mutable]
+        public long? ProcessId { get; set; }
+
+        [Member(5)]
+        public DateTime? Terminated { get; set; }
+
+        [Member(6), Timestamp]
+        public DateTime Modified { get; set; }
+    }
+}
