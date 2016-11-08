b8eb458ee4816592e1d928717c11fb4b641a56c5
Jason Nelson <jason@carbonmade.com>
2016-11-07 20:47:55 -0600

    Add containers

---
+++ Carbon.Platform/Containers/Container.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Carbon.Computing
+{
+    using Data.Annotations;
+
+    // Docker container...
+
+    [Dataset("Containers")]
+    public class Container : IHost
+    {
+        [Member(1), Identity]
+        public long Id { get; set; }
+
+        [Member(2)]
+        public string Name { get; set; }
+
+        [Member(3), Indexed]
+        public long HostId { get; set; }
+
+        [Member(4)]
+        public DateTime Created { get; set; }
+    }
+}
