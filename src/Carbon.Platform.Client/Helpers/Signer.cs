20d6fb56aeaf207920c5bb60476b95be054ddfa8
Jason Nelson <jason@carbonmade.com>
2016-12-06 16:31:02 -0600

    Add PlatformClient

---
+++ src/Carbon.Platform.Client/Helpers/Signer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace Carbon.Platform
+{
+    using Protection;
+    using Security;
+
+    public static class Signer
+    {
+        public static void SignRequest(SecretKey secret, HttpRequestMessage request)
+        {
+            #region Preconditions
+
+            if (secret.Value == null) throw new ArgumentNullException(nameof(secret.Value));
+
+            #endregion
+
+            request.Headers.Date = DateTimeOffset.UtcNow;
+            request.Headers.Add("User-Agent", "Carbon/1.0");
+
+            var dateHeader = request.Headers.GetValues("Date").First();
+            
+            var stringToSign = string.Join("\n",
+                "HMAC-SHA256",                       // Algorithm
+                dateHeader,                          // Request Date / TODO: Format as ISO
+                request.Method.ToString().ToUpper(),
+                request.RequestUri.Authority,
+                request.RequestUri.AbsolutePath
+            );
+
+            var signature = Signature.ComputeHmacSha256(
+                key  : secret,
+                data : Encoding.UTF8.GetBytes(stringToSign)
+            );
+
+            // TODO: Credential
+
+            var headerValue = $"C Algorithm=HMAC-SHA256,Signature={signature.ToHexString()}";
+
+            request.Headers.TryAddWithoutValidation("Authorization", headerValue);
+        }
+    }
+}
---
+++ src/Carbon.Platform.Client/PlatformClient.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Carbon.Platform
+{
+    using Apps;
+    using Computing;
+    using Frontends;
+    using Json;
+    using Networking;
+    using Protection;
+    using Storage;
+    using Versioning;
+
+    public class PlatformClient
+    {
+        public PlatformClient(string host, SecretKey secret)
+        {
+            Secret = secret;
+            Host   = host;
+
+            Apps              = new AppsClient(this);
+            Hosts             = new HostsClient(this);
+            Frontends         = new FrontendsClient(this);
+            NetworkInterfaces = new Dao<NetworkInterfaceInfo>("networkinterfaces", this);
+            Volumes           = new Dao<VolumeInfo>("volumes", this);
+        }
+
+        internal string Host { get; } // e.g. platform.myservice.com
+
+        internal SecretKey Secret { get; }
+
+        #region Apps
+
+        public AppsClient Apps { get; }
+
+        public class AppsClient : Dao<App>
+        {
+            public AppsClient(PlatformClient platform)
+                : base("apps", platform) { }
+
+            public Task<AppRelease> GetReleaseAsync(long appId, SemanticVersion version)
+                => GetAsync<AppRelease>($"apps/{appId}/releases/{version}");
+
+            // Create
+            // Delete
+            // RegisterInstance
+            // DeregisterInstance
+            // CreateRelease
+        }
+
+
+        #endregion
+
+        #region Hosts
+
+        public HostsClient Hosts { get; }
+
+        public class HostsClient : Dao<Host>
+        {
+            public HostsClient(PlatformClient platform)
+                : base("hosts", platform) { }
+
+            public Task<Host> GetAsync(PlatformProviderId provider, string refId)
+                => GetAsync<Host>("hosts/" + refId);
+
+            public Task<List<App>> GetAppsAsync(long id)
+                => GetAsync<List<App>>($"hosts/{id}/apps");
+
+            // Register
+        }
+
+
+        #endregion
+
+        #region Frontends
+
+        public FrontendsClient Frontends { get; }
+
+        public class FrontendsClient : Dao<Frontend>
+        {
+            public FrontendsClient(PlatformClient platform)
+                : base("frontends", platform) { }
+
+            public Task<AppRelease> GetRelease(long frontendId, SemanticVersion version)
+                => GetAsync<AppRelease>($"frontends/{frontendId}/releases/{version}");
+        }
+
+        #endregion
+
+        public Dao<NetworkInterfaceInfo> NetworkInterfaces { get; }
+
+        public Dao<VolumeInfo> Volumes { get; }
+
+        #region DAO Helper
+
+        public class Dao<T>
+            where T : new()
+        {
+            private readonly HttpClient httpClient = new HttpClient();
+
+            private readonly PlatformClient platform;
+            private readonly string prefix;
+             
+            public Dao(string prefix, PlatformClient platform)
+            {
+                this.prefix = prefix;
+                this.platform = platform;
+            }
+
+            public Task<T> GetAsync(long id)
+                => GetAsync<T>(prefix + "/" + id);
+
+            protected async Task<T1> GetAsync<T1>(string path)
+                where T1: new()
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, $"https://{platform.Host}/{path}") {
+                    Headers = {
+                        { "Accept", "application/json" }
+                    }
+                };
+
+                Signer.SignRequest(platform.Secret, request);
+
+                using (var response = await httpClient.SendAsync(request).ConfigureAwait(false))
+                {
+                    var text = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                    try
+                    {
+                        return JsonObject.Parse(text).As<T1>();
+                    }
+                    catch
+                    {
+                        throw new Exception("Error parsing:" + text);
+                    }
+                }
+            }
+        }
+    }
+
+    #endregion
+}
