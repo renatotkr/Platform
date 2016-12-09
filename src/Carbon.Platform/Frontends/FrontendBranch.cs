75b33c45fea88cdd13e1348be3ba4fb8507f279b
Jason Nelson <jason@carbonmade.com>
2016-12-09 15:57:02 -0600

    Add Frontend branches

---
+++ src/Carbon.Platform/Frontends/FrontendBranch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Carbon.Platform.Frontends
+{
+    using Data.Annotations;
+
+    [Dataset("FrontendBranches")]
+    public class FrontendBranch
+    {
+        public FrontendBranch() { }
+
+        public FrontendBranch(long frontendId, string name)
+        {
+            FrontendId = frontendId;
+            Name = name;
+        }
+
+        public FrontendBranch(long frontendId, string name, DateTime modified)
+        {
+            FrontendId = frontendId;
+            Name = name;
+            Modified = modified;
+        }
+
+        [Member("frontendId"), Key]
+        public long FrontendId { get; set; }
+
+        [Member("name"), Key]
+        public string Name { get; set; }
+
+        [Member("modified"), Timestamp]
+        public DateTime Modified { get; set; }
+    }
+}
