0183bfc16da724a6c280f9338cdf4efd47187688
Jason Nelson <jason@carbonmade.com>
2017-03-10 20:29:56 -0600

    Add Outage

---
+++ src/Carbon.Platform/Outages/Outage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Carbon.Platform
+{
+    using Data.Annotations;
+
+    [Dataset("Outages")]
+    public class Outage 
+    {
+        [Member("id"), Key]
+        public long Id { get; set; }
+
+        [Member("locationId")]
+        public long LocationId { get; set; }
+
+        [Member("scope")] // Effected services
+        public ResourceType Scope { get; set; }
+
+        [Member("resolved")]
+        public DateTime? Resolved { get; set; }
+
+        [Member("created")]
+        public DateTime Created { get; set; }
+
+        [Member("modified"), Timestamp(true)]
+        public DateTime Modified { get; set; }
+
+        #region IResource
+
+        public CloudProvider Provider => LocationHelper.GetProvider(LocationId);
+
+        #endregion
+    }
+}
