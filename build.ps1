f78f5b99fd35aff9960e6d2cca7815da0a6c3b2f
Jason Nelson <jason@carbonmade.com>
2017-03-10 20:32:30 -0600

    Use Release configuration for builds

--- build.ps1
+++ build.ps1
@@ -1,11 +1,12 @@
-dotnet pack ./src/GitHub                   -o ./build/GitHub
-dotnet pack ./src/Bitbucket                -o ./build/Bitbucket
-dotnet pack ./src/Carbon.Building          -o ./build/Carbon.Building
-dotnet pack ./src/Carbon.Building.Web      -o ./build/Carbon.Building.Web
-dotnet pack ./src/Carbon.Hosting           -o ./build/Carbon.Hosting
-dotnet pack ./src/Carbon.Packaging         -o ./build/Carbon.Packaging
-dotnet pack ./src/Carbon.Platform          -o ./build/Carbon.Platform
-dotnet pack ./src/Carbon.Platform.Services -o ./build/Carbon.Platform.Services
-dotnet pack ./src/Carbon.Platform.Runtime  -o ./build/Carbon.Platform.Runtime
-dotnet pack ./src/Carbon.Platform.Client   -o ./build/Carbon.Platform.Client
-dotnet pack ./src/Carbon.Libraries         -o ./build/Carbon.Libraries
+dotnet pack ./src/GitHub                    -c Release -o ./../../build/GitHub
+dotnet pack ./src/Bitbucket                 -c Release -o ./../../build/Bitbucket
+dotnet pack ./src/Carbon.Building           -c Release -o ./../../build/Carbon.Building
+dotnet pack ./src/Carbon.Building.Web       -c Release -o ./../../build/Carbon.Building.Web
+dotnet pack ./src/Carbon.Hosting            -c Release -o ./../../build/Carbon.Hosting
+dotnet pack ./src/Carbon.Packaging          -c Release -o ./../../build/Carbon.Packaging
+dotnet pack ./src/Carbon.Platform           -c Release -o ./../../build/Carbon.Platform
+dotnet pack ./src/Carbon.Platform.Frontends -c Release -o ./../../build/Carbon.Platform.Frontends
+dotnet pack ./src/Carbon.Platform.Services  -c Release -o ./../../build/Carbon.Platform.Services
+dotnet pack ./src/Carbon.Platform.Runtime   -c Release -o ./../../build/Carbon.Platform.Runtime
+dotnet pack ./src/Carbon.Platform.Client    -c Release -o ./../../build/Carbon.Platform.Client
+dotnet pack ./src/Carbon.Libraries          -c Release -o ./../../build/Carbon.Libraries
