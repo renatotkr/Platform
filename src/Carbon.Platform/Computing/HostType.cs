cd853d3b239370a3f59468a7748f3958afc2dd8c
Jason Nelson <jason@carbonmade.com>
2017-03-31 10:51:07 -1100

    More a few more things to Abstractions

--- src/Carbon.Platform/Computing/HostType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Carbon.Platform.Computing
-{
-    public enum HostType : byte
-    {
-        Physical  = 1, // Metal
-        Virtual   = 2, // Virtualized
-        Container = 3, // Containerized
-    }
-}
