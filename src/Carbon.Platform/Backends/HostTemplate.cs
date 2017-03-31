00bc14ce5b10a1730e27432119d46550b5ed7125
Jason Nelson <jason@carbonmade.com>
2017-03-30 14:46:06 -1100

    Add HostTemplate

---
+++ src/Carbon.Platform/Backends/HostTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+using Carbon.Data.Annotations;
+using Carbon.Json;
+
+namespace Carbon.Platform.Computing
+{
+    [Dataset("HostTemplates")]
+    public class HostTemplate : IHostTemplate
+    {
+        [Member("id"), Key]
+        public long Id { get; set; }
+
+        [Member("machineTypeId")]
+        public long MachineTypeId { get; set; }
+
+        [Member("machineImageId")]
+        public long MachineImageId { get; set; }
+
+        // Script at startup
+        [Member("script")]
+        public string Script { get; set; }
+
+        [Member("metadata")]
+        [StringLength(1000)]
+        public JsonObject Metadata { get; set; }
+
+        [IgnoreDataMember]
+        [Member("created"), Timestamp]
+        public DateTime Created { get; }
+    }
+}
