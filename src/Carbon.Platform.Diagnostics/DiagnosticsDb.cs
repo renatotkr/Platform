202f9034e7b816db1dbcdfbf00fb4ea6db72d089
Jason Nelson <jason@carbonmade.com>
2017-04-11 12:45:33 -1100

    Add DiagnosticsDb

---
+++ src/Carbon.Platform.Diagnostics/DiagnosticsDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Carbon.Data;
+
+namespace Carbon.Platform.Diagnostics
+{
+    public class DiagnosticsDb
+    {
+        public DiagnosticsDb(IDbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+
+            context.Types.TryAdd(new BigIdHandler());
+
+            Exceptions = new Dataset<ServerException, long>(context);
+        }
+
+        public IDbContext Context { get; }
+
+        public Dataset<ServerException, long> Exceptions { get; }
+    }
+}
