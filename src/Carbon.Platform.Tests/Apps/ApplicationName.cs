d34ada688ff008e2dc79c63cd5abd23223f8a549
Jason Nelson <jason@carbonmade.com>
2017-04-11 05:19:49 -1100

    Add ApplicationName test

---
+++ src/Carbon.Platform.Tests/Apps/ApplicationName.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Carbon.Platform.Apps.Tests
+{
+    /*
+    public class ApplicationNameTests
+    {
+        // forbidden characters in IIS
+        static readonly char[] forbiddenChars = {
+            '&', '/', '\\', ':', '*', '?', '|', '<', '>', '[', ']', '+', '=', ';', ',', '@'
+        };
+
+        [Fact]
+        public void Validate()
+        {
+            Assert.True(ApplicationName.Validate("carbon"));
+            Assert.True(ApplicationName.Validate("carbon_a"));
+            Assert.True(ApplicationName.Validate("carbon_b"));
+
+            foreach (var c in forbiddenChars)
+            {
+                Assert.False(ApplicationName.Validate("app" + c));    
+            }
+
+            // 65 characters
+            Assert.False(ApplicationName.Validate("carboncarboncarboncarboncarboncarboncarboncarboncarboncarboncarboncarboncarbon"));
+
+        }
+
+    }
+    */
+}
