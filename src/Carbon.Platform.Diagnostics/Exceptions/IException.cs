faef61c0a168028f5e2e4c6f5ef509aa7de16336
Jason Nelson <jason@carbonmade.com>
2017-04-11 03:29:07 -1100

    Breakout Diagnostics

---
+++ src/Carbon.Platform.Diagnostics/Exceptions/IException.cs
@@ -0,0 +1,31 @@
+﻿using Carbon.Json;
+
+namespace Carbon.Platform.Diagnostics
+{
+    public interface IException
+    {
+        // Details
+
+        string Type { get; }
+
+        string Message { get; }
+
+        JsonObject Details { get; }
+
+        // RequestId
+
+        long? SessionId { get; }
+    }
+}
+
+
+/*
+{
+  "method": string,
+  "url": string,
+  "userAgent": string,
+  "referrer": string,
+  "responseStatusCode": number,
+  "remoteIp": string,
+}
+*/
---
+++ src/Carbon.Platform.Diagnostics/Helpers/BigId.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Carbon.Platform.Diagnostics.Identity
+{
+    [StructLayout(LayoutKind.Explicit, Size = 16)]
+    public struct BigId : IEquatable<BigId>
+    {
+        [FieldOffset(0)]
+        public long scopeId;
+
+        // sinces since 1970 -- gives us until 2,106
+        [FieldOffset(8)]
+        public uint timestamp;
+
+        [FieldOffset(12)]
+        public uint sequence;
+
+        public long ScopeId => scopeId;
+
+        public DateTime GetTimestamp()
+        {
+            return DateTimeOffset.FromUnixTimeSeconds(timestamp).UtcDateTime;
+        }
+
+        public byte[] Serialize()
+        {
+            byte[] data = new byte[16];
+
+            var a = BitConverter.GetBytes(scopeId);
+            var b = BitConverter.GetBytes(timestamp);
+            var c = BitConverter.GetBytes(sequence);
+
+            Array.Copy(a, 0, data, 0, 8);
+            Array.Copy(b, 0, data, 8, 4);
+            Array.Copy(c, 0, data, 12, 4);
+
+            return data;
+        }
+
+        public static BigId Parse(string text)
+        {
+            var bytes = Convert.FromBase64String(text);
+
+            return Deserialize(bytes);
+        }
+
+        public override string ToString()
+        {
+            return Convert.ToBase64String(Serialize());
+        }
+
+        public static BigId Create(long scopeId, uint timestamp, uint sequence) =>
+            new BigId
+            {
+                scopeId = scopeId,
+                timestamp = timestamp,
+                sequence = sequence
+            };
+
+        public static BigId Deserialize(byte[] data) =>
+            new BigId
+            {
+                scopeId = BitConverter.ToInt64(data, 0),
+                timestamp = BitConverter.ToUInt32(data, 8),
+                sequence = BitConverter.ToUInt32(data, 12)
+            };
+
+        public bool Equals(BigId other) =>
+            scopeId == other.scopeId &&
+            timestamp == other.timestamp &&
+            sequence == other.sequence;
+    }
+
+    // Timestamp = 48
+    // Random    = 16 
+}
