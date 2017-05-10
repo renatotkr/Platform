76a57eb8645c34c0cdb360f3b7b3b2af6803bf82
Jason Nelson <jason@carbonmade.com>
2017-05-10 16:59:31 -0700

    Reverse environment types

--- src/Carbon.Platform.Abstractions/Environments/EnvironmentType.cs
+++ src/Carbon.Platform.Abstractions/Environments/EnvironmentType.cs
@@ -2,9 +2,9 @@
 {
     public enum EnvironmentType : byte
     {
-        Development  = 1,
-        Intergration = 2,
-        Staging      = 3,
-        Production   = 4
+        Production   = 1,
+        Staging      = 2,
+        Intergration = 3,
+        Development  = 4
     }
 }
