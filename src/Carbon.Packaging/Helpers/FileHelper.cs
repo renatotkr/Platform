712eb573aefa7e6074a4f99d4b7484fd26e8067a
Jason Nelson <jason@carbonmade.com>
2017-05-11 11:41:41 -0700

    Add woff2 support

--- src/Carbon.Packaging/Helpers/FileHelper.cs
+++ src/Carbon.Packaging/Helpers/FileHelper.cs
@@ -7,13 +7,11 @@ namespace Carbon.Packaging
         private static readonly string[] txtFormats = { "css", "html", "js", "mtpl", "tpl", "txt" };
 
         private static readonly string[] staticFormats = {
-            "css", "eot", "gif", "html", "ico", "jpeg", "jpg", "js", "png", "svg", "swf", "ttf", "webm", "webp", "woff"
+            "css", "eot", "gif", "html", "ico", "jpeg", "jpg", "js", "png", "svg", "swf", "ttf", "webm", "webp", "woff", "woff2"
         };
 
-        public static bool IsText(string format) => 
-            Array.BinarySearch(txtFormats, format) > 0;
+        public static bool IsText(string format) =>  Array.BinarySearch(txtFormats, format) > 0;
 
-        public static bool IsStatic(string format) => 
-            Array.BinarySearch(staticFormats, format) > -1;
+        public static bool IsStatic(string format) => Array.BinarySearch(staticFormats, format) > -1;
     }
 }
