5e005a5873540b5609b62e042b908077e6d6b5a7
Jason Nelson <jason@carbonmade.com>
2017-05-16 19:47:38 -0700

    Move builds to platform

---
+++ src/Carbon.Platform.Abstractions/CI/BuildStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Carbon.Platform.CI
+{
+    public enum BuildStatus
+    {
+        Pending    = 1,
+        Building   = 2,
+        Completed  = 3,
+        Failed     = 5
+    }
+}
---
+++ src/Carbon.Platform/CI/Build.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+using Carbon.Data.Annotations;
+using Carbon.Platform.Resources;
+
+namespace Carbon.Platform.CI
+{
+    [Dataset("Builds")]
+    public class Build : IBuild, IManagedResource
+    {
+        public Build() { }
+
+        public Build(long id, long commitId, long creatorId, ManagedResource resource)
+        {
+            Id        = id;
+            CommitId  = commitId;
+            CreatorId = creatorId;
+            
+            ProviderId = resource.ProviderId;
+            ResourceId = resource.ResourceId;
+            LocationId = resource.LocationId;
+        }
+
+        [Member("id"), Key(sequenceName: "buildId")]
+        public long Id { get; }
+
+        [Member("status"), Mutable]
+        public BuildStatus Status { get; set; }
+        
+        [Member("commitId")]
+        public long CommitId { get; }
+        
+        // ErrorMessage
+
+        // Artifacts
+
+        [Member("duration"), Mutable]
+        public TimeSpan? Duration { get; set; }
+        
+        [Member("creatorId")]
+        public long CreatorId { get; }
+
+        #region IResource
+
+        [Member("providerId")]
+        public int ProviderId { get; set; }
+
+        [Member("resourceId")]
+        public string ResourceId { get; set; }
+
+        [Member("locationId")]
+        public int LocationId { get; set; }
+        
+        ResourceType IResource.ResourceType => ResourceType.Build;
+
+        #endregion
+
+        #region Timestamps
+
+        [Member("created"), Timestamp]
+        public DateTime Created { get; }
+
+        [Member("modified"), Timestamp(true)]
+        public DateTime Modified { get; }
+
+        [Member("started"), Mutable]
+        public DateTime? Started { get; set; }
+
+        [Member("completed"), Mutable]
+        public DateTime? Completed { get; set; }
+
+        #endregion
+    }
+}
