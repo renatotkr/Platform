c25c98b6e35a85f59886429cb5a1dcf82511933c
Jason Nelson <jason@carbonmade.com>
2017-05-17 12:00:30 -0700

    Expand user model

--- src/Carbon.Platform/Iam/User.cs
+++ src/Carbon.Platform/Iam/User.cs
@@ -2,30 +2,37 @@
 using System.Runtime.Serialization;
 
 using Carbon.Data.Annotations;
+using Carbon.Json;
 using Carbon.Platform.Resources;
 
 namespace Carbon.Platform.Iam
 {
     [Dataset("Users")]
+    // [DataIndex(IndexFlags.Unique, "organizationId", "name")]
     public class User : IResource
     {
         public User() { }
 
-        public User(long id, string name)
+        public User(long id, string name, long? organizationId = null)
         {
-            Id   = id;
-            Name = name ?? throw new ArgumentNullException(nameof(name));
+            Id             = id;
+            Name           = name ?? throw new ArgumentNullException(nameof(name));
+            OrganizationId = organizationId;
         }
 
-        [Member("id")]
-        [Key(sequenceName: "userId")] // TODO: Carve out a small range...
+        [Member("id"), Key(sequenceName: "userId")]
         public long Id { get; }
-
-        // scope name to directory?
 
         [Member("name")]
         [StringLength(63)]
         public string Name { get; }
+
+        [Member("organizationId")]
+        public long? OrganizationId { get; }
+
+        [Member("details")]
+        [StringLength(1000)]
+        public JsonObject Details { get; set; }
 
         #region IResource
 
@@ -44,18 +51,22 @@ namespace Carbon.Platform.Iam
         public DateTime? Modified { get; }
 
         [IgnoreDataMember]
+        [Member("suspended")]
+        [TimePrecision(TimePrecision.Second)]
+        public DateTime? Suspended { get; }
+
+        [IgnoreDataMember]
         [Member("deleted")]
         [TimePrecision(TimePrecision.Second)]
         public DateTime? Deleted { get; }
 
         #endregion
     }
+
+    public static class UserProperties
+    {
+        public const string Oid     = "oid"; // Directories
+        public const string Uid     = "uid"; // Linux users
+        public const string IamRole = "iamRole";
+    }
 }
-
-
-// oid
-
-// A user may have mutiple identities
-// e.g. Iam, Borg, Google, ...
-
-// IAM: max name = 64 characters
