29e9d304113a85e15babd13b7d519fd98e74a283
Jason Nelson <jason@carbonmade.com>
2017-05-17 16:37:36 -0700

    Add BuildService

---
+++ src/Carbon.Platform.CI/Services/CreateBuildRequest.cs
@@ -0,0 +1,16 @@
+﻿using Carbon.Platform.Resources;
+
+namespace Carbon.Platform.CI
+{
+    public class CreateBuildRequest
+    {
+        public long RepositoryId { get; set; }
+
+        // e.g. 6dcb09b5b57875f334f61aebed695e2e4193db5e
+        public string Revision { get; set; }
+
+        public long CreatorId { get; set; }
+
+        public ManagedResource Resource { get; set; }
+    }
+}
