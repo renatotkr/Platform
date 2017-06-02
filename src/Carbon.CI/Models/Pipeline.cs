0adb51686d00c99cf6e19ea5be7dc6e3f2e8d5a6
Jason Nelson <jason@carbonmade.com>
2017-06-02 12:18:34 -0700

    Begin pipelines

---
+++ src/Carbon.CI/Models/Pipeline.cs
@@ -0,0 +1,43 @@
+﻿using Carbon.Data.Annotations;
+
+namespace Carbon.CI
+{
+    [Dataset("Pipelines")]
+    public class Pipeline
+    {
+        public Pipeline() { }
+
+        public Pipeline(long id)
+        {
+            Id = id;
+        }
+
+        [Member("id"), Key(sequenceName: "pipelineId")]
+        public long Id { get; }
+
+        [Member("name")]
+        public string Name { get; set; }
+
+        // trigger
+    }
+
+    // download source
+    // build 
+    // test
+    // deploy
+    // ...
+
+    [Dataset("PipelineStages")]
+    public class PipelineStage
+    {
+        // pipelineId | #
+        [Member("id"), Key]
+        public long Id { get; }
+
+        [Member("name")]
+        public string Name { get; set; }
+    }
+
+
+    // PipelineRun...
+}
