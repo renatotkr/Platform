9545868ba8d14af099bded32d187df70e9b807d9
Jason Nelson <jason@carbonmade.com>
2017-07-17 09:22:48 -0700

    Add HostTemplateService

---
+++ src/Carbon.Platform.Services/Computing/HostTemplateService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Carbon.Data.Expressions;
+using Carbon.Platform.Resources;
+
+namespace Carbon.Platform.Computing
+{
+    public class HostTemplateService : IHostTemplateService
+    {
+        private readonly PlatformDb db;
+
+        public HostTemplateService(PlatformDb db)
+        {
+            this.db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        public Task<IReadOnlyList<HostTemplate>> ListAsync()
+        {
+            return db.HostTemplates.QueryAsync(Expression.IsNull("deleted"));
+        }
+
+        public async Task<HostTemplate> GetAsync(long id)
+        {
+            return await db.HostTemplates.FindAsync(id).ConfigureAwait(false) 
+                ?? throw ResourceError.NotFound(ResourceTypes.Image, id);
+        }
+
+        public async Task<HostTemplate> CreateAsync(CreateHostTemplateRequest request)
+        {
+            #region Preconditions
+            
+            Validate.Object(request, nameof(request));
+
+            #endregion
+            
+            var templateId = await db.HostTemplates.Sequence.NextAsync();
+
+            var template = new HostTemplate(
+                id            : templateId,
+                ownerId       : request.OwnerId,
+                name          : request.Name,
+                machineType   : request.MachineType,
+                image         : request.Image,
+                locationId    : request.LocationId,
+                startupScript : request.StartupScript,
+                properties    : request.Properties
+            );
+
+            await db.HostTemplates.InsertAsync(template).ConfigureAwait(false);
+
+            return template;
+        }
+    }
+}
---
+++ src/Carbon.Platform.Services/Computing/IHostTemplateService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Carbon.Platform.Computing
+{
+    public interface IHostTemplateService
+    {
+        Task<HostTemplate> CreateAsync(CreateHostTemplateRequest request);
+
+        Task<HostTemplate> GetAsync(long id);
+
+        Task<IReadOnlyList<HostTemplate>> ListAsync();
+    }
+}
