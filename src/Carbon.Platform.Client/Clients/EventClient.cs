f655a9411f4319463bebbc012505d85968def684
Jason Nelson <jason@carbonmade.com>
2017-08-11 16:57:44 -0700

    Update deps

---
+++ src/Carbon.Platform.Client/Clients/EventClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Carbon.Data.Expressions;
+using Carbon.Data.Sequences;
+
+namespace Carbon.Platform
+{
+    public class EventClient
+    {
+        private readonly ApiBase api;
+
+        internal EventClient(ApiBase api)
+        {
+            this.api = api ?? throw new ArgumentNullException(nameof(api));
+        }
+
+        public Task<EventDetails[]> ListAsync(Expression filter = null)
+        {
+            return api.GetListAsync<EventDetails>($"/events" + filter?.ToQueryString());
+        }
+
+        public Task<EventDetails> GetAsync(Uid id)
+        {
+            return api.GetAsync<EventDetails>($"/events/{id}");
+        }
+        
+        public Task<EventDetails> CreateAsync(EventDetails record)
+        {
+            return api.PostAsync<EventDetails>($"/events", record);
+        }
+    }
+}
---
+++ src/Carbon.Platform.Client/Models/EventDetails.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+using Carbon.Data.Sequences;
+using Carbon.Json;
+
+namespace Carbon.Platform
+{
+    public class EventDetails
+    {
+        [DataMember(Name = "properties", EmitDefaultValue = false)]
+        public Uid? Id { get; set; }
+
+        [DataMember(Name = "action", EmitDefaultValue = false)]
+        public string Action { get; set; }
+
+        [DataMember(Name = "resource", EmitDefaultValue = false)]
+        public string Resource { get; set; }
+
+        [DataMember(Name = "context", EmitDefaultValue = false)]
+        public JsonObject Context { get; set; }
+
+        [DataMember(Name = "properties", EmitDefaultValue = false)]
+        public JsonObject Properties { get; set; }
+    }
+}
