434d0953fd45f1ca274832abd7ac104beaae6a38
Jason Nelson <jason@carbonmade.com>
2017-10-07 14:47:09 -0700

    Add missing domain members

---
+++ src/Carbon.Platform/Hosting/DomainRecordFlags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Carbon.Platform.Hosting
+{
+    [Flags]
+    public enum DomainRecordFlags
+    {
+        None     = 0,
+        Alias    = 1 << 0,
+        Wildcard = 1 << 1
+    }
+}
