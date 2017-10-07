6769afcb608e100e5af9cd86948b5efdbdf08826
Jason Nelson <jason@carbonmade.com>
2017-10-07 14:39:09 -0700

    Make ResourceTypes readonly

--- src/Carbon.Platform.Abstractions/Resources/ResourceType.cs
+++ src/Carbon.Platform.Abstractions/Resources/ResourceType.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Carbon.Platform.Resources
 {
+    [DataContract]
     public struct ResourceType : IEquatable<ResourceType>
     {
         public ResourceType(string name)
@@ -9,6 +11,7 @@ namespace Carbon.Platform.Resources
             Name = name ?? throw new ArgumentNullException(nameof(name));
         }
 
+        [DataMember(Name = "name", Order = 1)]
         public string Name { get; }
 
         public override string ToString() => Name;
@@ -31,89 +34,89 @@ namespace Carbon.Platform.Resources
     public static class ResourceTypes
     {
         // IAM --------------------------------------------------------------------------------------------
-        public static ResourceType Account       = "account";
-        public static ResourceType Entity        = "entity";   // Organization, Person, or IA
-        public static ResourceType User          = "user";      
-        public static ResourceType Session       = "session";
+        public static readonly ResourceType Account              = "account";
+        public static readonly ResourceType Entity               = "entity";   // Organization, Person, or IA
+        public static readonly ResourceType User                 = "user";      
+        public static readonly ResourceType Session              = "session";
 
         // Environments-------------------------------------------------------------------------------------
 
-        public static ResourceType Environment   = "environment"; // An environment actions take place within
-        public static ResourceType Location      = "location";
+        public static readonly ResourceType Environment          = "environment"; // An environment actions take place within
+        public static readonly ResourceType Location             = "location";
 
         // Computing  -----------------------------------------------------------------------------------------
-        public static ResourceType Cluster              = "cluster";
-        public static ResourceType Host                 = "host";          //  metal, vms, and containers
-        public static ResourceType HostTemplate         = "host:template";
-        public static ResourceType Image                = "host:image";
-        public static ResourceType MachineType          = "machineType";
-        public static ResourceType Program              = "program";
-        public static ResourceType ProgramRelease       = "program:release";
-        public static ResourceType LoadBalancer         = "loadBalancer";
-        public static ResourceType LoadBalancerListener = "loadBalancer:listener";
-        public static ResourceType LoadBalancerRule     = "loadBalancer:rule";
+        public static readonly ResourceType Cluster              = "cluster";
+        public static readonly ResourceType Host                 = "host";          //  metal, vms, and containers
+        public static readonly ResourceType HostTemplate         = "host:template";
+        public static readonly ResourceType Image                = "host:image";
+        public static readonly ResourceType MachineType          = "machine`type";
+        public static readonly ResourceType Program              = "program";
+        public static readonly ResourceType ProgramRelease       = "program:release";
+        public static readonly ResourceType LoadBalancer         = "load`balancer";
+        public static readonly ResourceType LoadBalancerListener = "load`balancer:listener";
+        public static readonly ResourceType LoadBalancerRule     = "load`balancer:rule";
 
         // Storage --------------------------------------------------------------------------------------------
-        public static ResourceType Bucket              = "bucket";        
-        public static ResourceType Channel             = "channel";               // AKA Stream / Topic           
-        public static ResourceType Queue               = "queue";         
-        public static ResourceType Repository          = "repository";
-        public static ResourceType RepositoryBranch    = "repository:branch";
-        public static ResourceType RepositoryCommit    = "repository:commit"; 
-        public static ResourceType RepositoryTag       = "repository:tag";
-        public static ResourceType Volume              = "volume";
-        public static ResourceType VolumeSnapshot      = "volume:snapshot";
+        public static readonly ResourceType Bucket               = "bucket";        
+        public static readonly ResourceType Channel              = "channel";  // AKA stream / topic          
+        public static readonly ResourceType Queue                = "queue";         
+        public static readonly ResourceType Repository           = "repository";
+        public static readonly ResourceType RepositoryBranch     = "repository:branch";
+        public static readonly ResourceType RepositoryCommit     = "repository:commit"; 
+        public static readonly ResourceType RepositoryTag        = "repository:tag";
+        public static readonly ResourceType Volume               = "volume";
+        public static readonly ResourceType VolumeSnapshot       = "volume:snapshot";
 
         // Rds --------------------------------------------------------------------
-        public static ResourceType Database         = "database";
-        public static ResourceType DatabaseCluster  = "database:cluster";
-        public static ResourceType DatabaseInstance = "database:instance";
-        public static ResourceType DatabaseSchema   = "database:schema";
-        public static ResourceType DatabaseGrant    = "database:grant";
-        public static ResourceType DatabaseUser     = "database:user";
+        public static readonly ResourceType Database             = "database";
+        public static readonly ResourceType DatabaseCluster      = "database:cluster";
+        public static readonly ResourceType DatabaseInstance     = "database:instance";
+        public static readonly ResourceType DatabaseSchema       = "database:schema";
+        public static readonly ResourceType DatabaseGrant        = "database:grant";
+        public static readonly ResourceType DatabaseUser         = "database:user";
 
         // Vaults / KMS--------------------------------------------------------------------------------------------------
-        public static ResourceType Vault       = "vault";
-        public static ResourceType VaultGrant  = "vault:grant";
-        public static ResourceType VaultKey    = "vault:key";
-        public static ResourceType VaultSecret = "vault:secret";
+        public static readonly ResourceType Vault                = "vault";
+        public static readonly ResourceType VaultGrant           = "vault:grant";
+        public static readonly ResourceType VaultKey             = "vault:key";
+        public static readonly ResourceType VaultSecret          = "vault:secret";
 
-        public static ResourceType Certificate = "certificate";
+        public static readonly ResourceType Certificate          = "certificate";
 
         // Networking --------------------------------------------------------------------------------------------
 
-        public static ResourceType Network              = "network";
-        public static ResourceType NetworkAddress       = "network:address";
-        public static ResourceType NetworkInterface     = "network:nterface";
-        public static ResourceType NetworkPeer          = "network:peer";
-        public static ResourceType NetworkRouter        = "network:router";
-        public static ResourceType NetworkRoute         = "network:route";
-        public static ResourceType NetworkSecurityGroup = "network:securityGroup";
-        public static ResourceType Subnet               = "subnet";
+        public static readonly ResourceType Network              = "network";
+        public static readonly ResourceType NetworkAddress       = "network:address";
+        public static readonly ResourceType NetworkInterface     = "network:nterface";
+        public static readonly ResourceType NetworkPeer          = "network:peer";
+        public static readonly ResourceType NetworkRouter        = "network:router";
+        public static readonly ResourceType NetworkRoute         = "network:route";
+        public static readonly ResourceType NetworkSecurityGroup = "network:security`group";
+        public static readonly ResourceType Subnet               = "subnet";
 
-        
         // Web -----------------------------------------------------------------------------------------------
 
-        public static ResourceType Domain        = "domain";
-                            
-        public static ResourceType Website       = "web:site";
-        public static ResourceType WebComponent  = "web:component";
-        public static ResourceType WebLibrary    = "web:library";
-        
-        public static ResourceType DnsZone       = "dns:zone";
-        public static ResourceType DnsRecord     = "dns:record";
+        public static readonly ResourceType Domain               = "domain";
+        public static readonly ResourceType DomainRecord         = "domain:record";
+        public static readonly ResourceType DomainRegistration   = "domain:registration";
+                                                                 
+        public static readonly ResourceType Website              = "web:site";
+        public static readonly ResourceType WebComponent         = "web:component";
+        public static readonly ResourceType WebLibrary           = "web:library";
+                                                                 
+        public static readonly ResourceType DnsZone              = "dns:zone";
+        public static readonly ResourceType DnsRecord            = "dns:record";
 
         // Health --------------------------------------------------------------------------------------------
-        public static ResourceType HealthCheck  = "health:check";
+        public static readonly ResourceType HealthCheck          = "health`check";
 
         // CI ------------------------------------------------------------------------------------------------
-        public static ResourceType Build         = "ci:build";
-        public static ResourceType Project       = "ci:project";
-        public static ResourceType Deployment    = "ci:deployment";
+        public static readonly ResourceType Build                = "ci:build";
+        public static readonly ResourceType Project              = "ci:project";
+        public static readonly ResourceType Deployment           = "deployment";
 
         // Metrics --------------------------------------------------------------------------------------------
-        public static ResourceType Metric          = "metric";
-        public static ResourceType MetricDimension = "metric:dimension";
-
+        public static readonly ResourceType Metric               = "metric";
+        public static readonly ResourceType MetricDimension      = "metric:dimension";
     }
 }
