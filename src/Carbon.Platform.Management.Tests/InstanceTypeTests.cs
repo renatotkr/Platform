29594b3427e78a0781e2ac0252937329b4324244
Jason Nelson <jason@carbonmade.com>
2017-12-12 13:56:28 -0800

    Add M5Large to AwsInstanceTypes

---
+++ src/Carbon.Platform.Management.Tests/InstanceTypeTests.cs
@@ -0,0 +1,15 @@
+﻿using Carbon.Platform.Computing;
+using Xunit;
+
+namespace Carbon.Platform.Management.Tests
+{
+    public class InstanceTypeTests
+    {
+        [Fact]
+        public void A()
+        {
+            Assert.Equal(4362403853, AwsInstanceTypes.M5Large.Id);
+            Assert.Equal("m5.large", AwsInstanceType.Get(4362403853).Name);
+        }
+    }
+}
--- src/Carbon.Platform.Management/Aws/AwsInstanceTypes.cs
+++ src/Carbon.Platform.Management/Aws/AwsInstanceTypes.cs
@@ -4,10 +4,11 @@ namespace Carbon.Platform.Management
 {
     public static class AwsInstanceTypes
     {
+        public static readonly IMachineType T2Small  = AwsInstanceType.Get("t2.small");
         public static readonly IMachineType T2Medium = AwsInstanceType.Get("t2.medium");
         public static readonly IMachineType T2Large  = AwsInstanceType.Get("t2.large");
         public static readonly IMachineType P2XLarge = AwsInstanceType.Get("p2.xlarge"); // 0.900 /hour [demand] 0.45 [reserved] = ~ $648/m
         public static readonly IMachineType C5Large  = AwsInstanceType.Get("c5.large");
-
+        public static readonly IMachineType M5Large  = AwsInstanceType.Get("m5.large");
     }
 }
