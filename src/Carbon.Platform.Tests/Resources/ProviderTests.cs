865b58777c09657d21def2dc4811a1e1dfb60def
Jason Nelson <jason@carbonmade.com>
2018-01-08 09:43:08 -0800

    Extend resource provider test coverage

--- src/Carbon.Platform.Tests/Resources/ProviderTests.cs
+++ src/Carbon.Platform.Tests/Resources/ProviderTests.cs
@@ -5,39 +5,44 @@ namespace Carbon.Platform.Resources
 	public class ProviderTests
 	{
         [Theory]
-        [InlineData("borg",  "Borg",  1)]
-        [InlineData("aws",   "AWS",   2)] // Amazon Web Services
-        [InlineData("gcp",   "GCP",   3)] // Google Cloud Platform
-        [InlineData("azure", "Azure", 4)] // Microsoft Azure
+        [InlineData("borg",         "Borg",          1)]
+        [InlineData("aws",          "AWS",           2)] // Amazon Web Services
+        [InlineData("gcp",          "GCP",           3)] // Google Cloud Platform
+        [InlineData("azure",        "Azure",         4)] // Microsoft Azure
+        [InlineData("ibm",          "IBM",           5)] // IBM Cloud
+        [InlineData("digitalocean", "Digital Ocean", 10)] 
+        [InlineData("vultr",        "Vultr",         20)] 
+        [InlineData("gcore",        "GCore",         105)]
+        [InlineData("incero",       "Incero",        106)]
+        [InlineData("wasabi",       "Wasabi",        107)]
         public void CloudProviders(string code, string name, int id)
         {
             var provider = ResourceProvider.Parse(code);
 
+            Assert.Equal(id, provider.Id);
             Assert.Equal(name, provider.Name);
-            Assert.Equal(id,   provider.Id);
-        }
 
-        /*
-        [Theory]
-        [InlineData("github",    1000)]
-        [InlineData("bitbucket", 1001)]
-        public void RepositoryProviders(string code, int id)
-        {
-            var provider = ResourceProvider.Get(id);
-
-            Assert.Equal(code, provider.Code);
+            // Ensure we can lookup by id too
+            Assert.Equal(id, ResourceProvider.Get(id).Id);
         }
 
         [Theory]
-        [InlineData("braintree", 2000)]
-        [InlineData("paypal",    2001)]
-        [InlineData("stripe",    2002)]
+        [InlineData("paypal",   5000)]
+        [InlineData("braintree", 5001)]
+        [InlineData("stripe", 5002)]
         public void PaymentProviders(string code, int id)
         {
-            var provider = ResourceProvider.Get(id);
-
-            Assert.Equal(code, provider.Code);
+            Assert.Equal(code, ResourceProvider.Get(id).Code);
+            Assert.Equal(id, ResourceProvider.Parse(code).Id);
         }
-        */
+
+        [Theory]
+        [InlineData("github",    6000)]
+        [InlineData("bitbucket", 6001)]
+        public void RepositoryProviders(string code, int id)
+        {
+            Assert.Equal(code, ResourceProvider.Get(id).Code);
+            Assert.Equal(id, ResourceProvider.Parse(code).Id);
+        }
     }
 }
