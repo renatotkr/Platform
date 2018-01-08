6e9f3d75b77694e7c042c13f4d4fef924bc1238c
Jason Nelson <jason@carbonmade.com>
2018-01-08 09:39:31 -0800

    Add Vultr, Incero, and IBM as resource providers

--- src/Carbon.Platform.Abstractions/Resources/ResourceProvider.cs
+++ src/Carbon.Platform.Abstractions/Resources/ResourceProvider.cs
@@ -10,36 +10,38 @@ namespace Carbon.Platform
         public static readonly ResourceProvider Aws          = new ResourceProvider(2,  "aws",   "AWS",   "amazonaws.com");
         public static readonly ResourceProvider Gcp          = new ResourceProvider(3,  "gcp",   "GCP",   "cloud.google.com");
         public static readonly ResourceProvider Azure        = new ResourceProvider(4,  "azure", "Azure", "azure.microsoft.com");
-        // 5 = softlayer
+        public static readonly ResourceProvider IBM          = new ResourceProvider(5,  "ibm",   "IBM",   "ibm.com"); // Formally softlayer
 
-        public static readonly ResourceProvider DigitalOcean = new ResourceProvider(10, "do",    "Digital Ocean", "digitalocean.com");
+        public static readonly ResourceProvider DigitalOcean = new ResourceProvider(10,  "digitalocean", "Digital Ocean", "digitalocean.com");
+        public static readonly ResourceProvider Vultr        = new ResourceProvider(20,  "vultr",  "Vultr", "vultr.com");
         
-        public static readonly ResourceProvider GCore        = new ResourceProvider(105,  "gcore",  "GCore",  "gcore.lu");
-
-        public static readonly ResourceProvider Wasabi       = new ResourceProvider(107,  "wasabi", "Wasabi", "wasabi.com");
+        public static readonly ResourceProvider GCore        = new ResourceProvider(105, "gcore",  "GCore",  "gcore.lu");
+        public static readonly ResourceProvider Incero       = new ResourceProvider(106, "incero", "Incero", "incero.com");
+        public static readonly ResourceProvider Wasabi       = new ResourceProvider(107, "wasabi", "Wasabi", "wasabi.com");
 
         // < 127
 
-        // Banks
+        // Banks`
+        public static readonly ResourceProvider PayPal    = new ResourceProvider(5000, "paypal", "PayPal", "paypal.com");
         public static readonly ResourceProvider Braintree = new ResourceProvider(5001, "braintree", "Braintree", "braintreepayments.com");
-        public static readonly ResourceProvider PayPal    = new ResourceProvider(5000, "paypal",    "PayPal", "paypal.com");
         public static readonly ResourceProvider Stripe    = new ResourceProvider(5002, "stripe",    "Stripe", "stripe.com");
 
         // Code Repository Providers 
         public static readonly ResourceProvider GitHub    = new ResourceProvider(6000, "github", "GitHub", "github.com");
         public static readonly ResourceProvider Bitbucket = new ResourceProvider(6001, "bitbucket", "Bitbucket", "bitbucket.org");
 
-        // Certificates = 3000
-        // public static readonly ResourceProvider LetEncrypt = new ResourceProvider(3000, "letsencrypt", "Let’s Encrypt");
-
+        // TODO: Let's Encrypt
 
         public static readonly Dictionary<int, ResourceProvider> map = new Dictionary<int, ResourceProvider> {
             { 1,    Borg },
             { 2,    Aws },
             { 3,    Gcp },
             { 4,    Azure },
+            { 5,    IBM },
             { 10,   DigitalOcean },
+            { 20,   Vultr },
             { 105,  GCore },
+            { 106,  Incero },
             { 107,  Wasabi },
 
             { 5000,  PayPal },
@@ -86,34 +88,35 @@ namespace Carbon.Platform
 
         public static ResourceProvider Parse(string name)
         {
-            #region Preconditions
-
             if (name == null)
                 throw new ArgumentNullException(nameof(name));
 
             if (name.Length == 0)
                 throw new ArgumentException("Must not be empty", nameof(name));
 
-            #endregion
-
             switch (name.ToLower())
             {
-                case "borg"      : return Borg;
-                case "aws"       : return Aws;
-                case "gcp"       : return Gcp;
-                case "azure"     : return Azure;
-                case "do"        : return DigitalOcean;
+                case "borg"         : return Borg;         // 1
+                case "aws"          : return Aws;          // 2
+                case "gcp"          : return Gcp;          // 3
+                case "azure"        : return Azure;        // 4
+                case "ibm"          : return IBM;          // 5
+                case "digitalocean" : return DigitalOcean; // 10
+                case "vultr"        : return Vultr;        // 20
+                case "gcore"        : return GCore;        // 105
+                case "incero"       : return Incero;       // 106
+                case "wasabi"       : return Wasabi;       // 107
 
-                // Full names
+                // Full names    
                 case "bitbucket" : return Bitbucket;
                 case "github"    : return GitHub;
-
+                                 
                 case "braintree" : return Braintree;
                 case "paypal"    : return PayPal;
                 case "stripe"    : return Stripe;
+            }
 
-                default: throw new Exception($"Unexpected provider '{name}'");
-            } 
+            throw new Exception($"Invalid provider name '{name}'");
         }
     }
 }
