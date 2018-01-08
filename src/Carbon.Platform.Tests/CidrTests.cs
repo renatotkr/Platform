fd15171b20794a55c4a24abc2e49e6ecc0f99838
Jason Nelson <jason@carbonmade.com>
2018-01-08 09:42:48 -0800

    Add Cidr test coverage

---
+++ src/Carbon.Platform.Tests/CidrTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Xunit;
+
+namespace Carbon.Net.Tests
+{
+    public class CidrTests
+    {
+        [Fact]
+        public void A()
+        {
+            var a = Cidr.Parse("92.223.124.5/32");
+            
+            Assert.Equal(IPAddress.Parse("92.223.124.5"), a.Start);
+            Assert.Equal(IPAddress.Parse("92.223.124.5"), a.End);
+        }
+
+        [Fact]
+        public void B()
+        {
+            var a = Cidr.Parse("92.223.126.204/32");
+
+            Assert.Equal(32, a.Suffix);
+            Assert.Equal(IPAddress.Parse("92.223.126.204"), a.Start);
+            Assert.Equal(IPAddress.Parse("92.223.126.204"), a.End);
+        }
+    }
+}
