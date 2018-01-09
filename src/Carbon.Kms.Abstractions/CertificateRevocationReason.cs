edf7f33bbd93674a824d9cf180529652ac03613b
Jason Nelson <jason@carbonmade.com>
2018-01-09 15:19:16 -0800

    Add CertificateRevocationReason

---
+++ src/Carbon.Kms.Abstractions/CertificateRevocationReason.cs
@@ -0,0 +1,16 @@
+﻿namespace Carbon.Kms
+{
+    public enum CertificateRevocationReason
+    {
+        Unspecified          = 0,
+        KeyCompromise        = 1,
+        CACompromise         = 2,
+        AffilationChanged    = 3,
+        Superseded           = 4,
+        CessationOfOperation = 5,
+        CertificateHold      = 6, 
+        RevokeFromCLR        = 8,
+        PrivilegeWithdrawn  = 9,
+        AACompromise        = 10
+    }
+}
